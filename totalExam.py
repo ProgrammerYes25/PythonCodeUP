49c5f74a076230c5c64bcb8db6239c7246b7b256
yscho010612@gmail.com <yscho010612@gmail.com>
2022-06-28 08:26:21 +0900

    Code up 종합 문제 풀이(78)

--- totalExam.py
+++ totalExam.py
@@ -1,12 +1,24 @@
-#6077 : [기초-종합] 짝수 합 구하기(설명)(py)
-#정수(1 ~ 100) 1개를 입력받아 1부터 그 수까지 짝수의 합을 구해보자.
+# #6077 : [기초-종합] 짝수 합 구하기(설명)(py)
+# #정수(1 ~ 100) 1개를 입력받아 1부터 그 수까지 짝수의 합을 구해보자.
+#
+# def total(i):
+#     tot = 0
+#     for n in range(1, i+1):
+#         if n%2==0:
+#             tot+=n;
+#     return tot;
+#
+# n = int(input())
+# print(total(n))
 
-def total(i):
-    tot = 0
-    for n in range(1, i+1):
-        if n%2==0:
-            tot+=n;
-    return tot;
+#6078 : [기초-종합] 원하는 문자가 입력될 때까지 반복 출력하기(py)
 
-n = int(input())
-print(total(n))
+def strin(ch):
+    s = 'q'
+    while ch != s:
+        ch = input()
+        print(ch)
+
+ch = input()
+print(ch)
+strin(ch)
